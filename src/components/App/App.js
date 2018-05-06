d3801fe10682a002e869e5abb815dc584d037385
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2018-05-07 01:30:54 +0200

    master: implements App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import InitialGamePanel from '../InitialGamePanel';
+import PlayerSelectionPanel from '../PlayerSelectionPanel';
+import BowlingGamePanel from '../BowlingGamePanel';
+import GameOverPanel from '../GameOverPanel';
+import { LAST_ROUND } from '../../utils/constants';
 
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     if (this.props.show === 'InitialGamePanel')
       return <InitialGamePanel />;
+
+    if (this.props.show === 'PlayerSelectionPanel')
+      return <PlayerSelectionPanel />;
+
+    if (this.props.gameRound === LAST_ROUND + 1)
+      return <GameOverPanel />;
+
+    return (
+      <BowlingGamePanel />
+    );
   }
 }
 
